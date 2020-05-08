291ee5af329db511a68c132c4679b0a6f01b1f34
neilmarshall <neil.marshall@dunelm.org.uk>
2020-05-08 12:51:27 +0100

    update selenium testing framework

--- tests/selenium_tests.py
+++ tests/selenium_tests.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 load_dotenv()
 CHROME_DRIVER = os.getenv("CHROME_DRIVER")
@@ -9,11 +12,19 @@ BABYNAMES_USERNAME = os.getenv("BABYNAMES_USERNAME")
 BABYNAMES_PASSWORD = os.getenv("BABYNAMES_PASSWORD")
 
 def convert_table_to_dict(driver, table_id):
-    map_score = lambda s: (s.split()[0], int(s.split()[1]))
-    scores = driver.find_element_by_id(table_id).text.split('\n')[2:]
-    return {name: score for name, score in map(map_score, scores)}
+    map_score = lambda s: (s.split()[0], 0 if s.split()[1] == '-' else int(s.split()[1]))
+    try:
+        scores = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, table_id))
+        )
+        scores = tuple(map(str.strip, scores.text.split('\n')))[2:]
+        return {name: score for name, score in map(map_score, scores)}
+    except Exception as e:
+        print(e)
+
 
 if __name__ == '__main__':
+
     driver = webdriver.Chrome(CHROME_DRIVER)
 
     try:
@@ -24,9 +35,8 @@ if __name__ == '__main__':
         driver.find_element_by_id("passwordInput").send_keys(BABYNAMES_PASSWORD)
         driver.find_element_by_class_name("btn").click()
 
-        breakpoint()
         results_table_1_A = convert_table_to_dict(driver, "results-table-1-A")
-        results_table_1_B = convert_table_to_dict(driver, "results-table-1-B")
+        results_table_2_A = convert_table_to_dict(driver, "results-table-2-A")
 
     finally:
         driver.quit()
