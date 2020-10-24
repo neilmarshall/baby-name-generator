c99e179c465cd9e449b733afe6e93038ffae187d
neilmarshall <neil.marshall@dunelm.org.uk>
2020-10-24 16:52:33 +0100

    update config to use Atlas DB

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    URL: process.env.MONGODB_URI || 'mongodb://localhost:27017/babynames',
+    URL: process.env.ATLASDB_URI || 'mongodb://localhost:27017/babynames',
     SECRET: process.env.SECRET || '8Jh6fd6U71A',
     PORT: process.env.PORT || 3000,
     userRoles: { ADMIN: "admin", USER: "user" }
